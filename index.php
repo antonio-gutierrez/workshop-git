1f606e177d202f62541e766c4e4fe17efa59b5ff
vercoza <vercoza_thays@id.uff.br>
2018-03-21 17:22:34 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<h3> Bem-vindos aos Execícios de Git</h3>
+
+<p>Complete a tabela abaixo com o nome do tópico ou sua descrição:</p>
+
+<table>
+    <thead>
+        <tr>
+            <th>Tópico</th>
+            <th>Descrição</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>&nbsp;</td>
+            <td>Área de arquivos locais não rastreados</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>Área de arquivos rastreados com modificações</td>
+        </tr>
+        <tr>
+            <td>Tracked</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Untracked</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Comando clone</td>
+            <td>Comando utilizado para baixar repozitório remoto</td>
+        </tr>
+         <tr>
+            <td>Comando init</td>
+            <td>Comando utilizado para iniciar um repozitórioitório local no diretório atual</td>
+        </tr>
+        <tr>
+            <td>Comando status</td>
+            <td>Comando utilizado para mostrar os estados dos arquivos do branch atual no repozitório local</td>
+        </tr>
+        <tr>
+            <td>Comando log</td>
+            <td>Comando utilizado para listar os commits do branch atual no repozitório local</td>
+        </tr>
+         <tr>
+            <td>Comando diff</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Comando add</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Comando commit</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Comando chekout</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>Comando utilizado para sincronizar o branch atual com repozitório remoto</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>Comando utilizado para enviar alterações do branch atual do repozitório local para o repozitório remoto</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>Comando utilizado para mesclar o branch local com outro branch determinado</td>
+        </tr>
+    </tbody>
+</table>
