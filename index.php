a73c1c29964c819cd1488fdf97394855ef7d4f6c
Danielly Alves <36994575+danniallves@users.noreply.github.com>
2018-03-22 16:27:33 -0300

    Alterando comandos git (#3)
    Conflitos resolvidos, ufa!

--- index.php
+++ index.php
@@ -36,36 +36,36 @@
             <td>Arquivo j&aacute; foi modificado e entrou na &aacute;rea de stage</td>
         </tr>
         <tr>
-            <td>Comando clone</td>
-            <td>git clone <url>)</td>
+            <td>git clone</td>
+            <td>Comando utilizado para baixar repozitório remoto</td>
         </tr>
          <tr>
-            <td>Comando init</td>
             <td>git init</td>
+            <td>Comando utilizado para iniciar um repozitórioitório local no diretório atual</td>
         </tr>
         <tr>
-            <td>Comando status</td>
             <td>git status</td>
+            <td>Comando utilizado para mostrar os estados dos arquivos do branch atual no repozitório local</td>
         </tr>
         <tr>
-            <td>Comando log</td>
             <td>git log</td>
+            <td>Comando utilizado para listar os commits do branch atual no repozitório local</td>
         </tr>
          <tr>
-            <td>Comando diff</td>
             <td>git diff</td>
+            <td>Comando utilizado para verificar as diferentes alterações entre arquivos </td>
         </tr>
         <tr>
             <td>Comando add</td>
-            <td>&nbsp;</td>
+            <td>Comando utilizado para adicionar na área de stage</td>
         </tr>
         <tr>
             <td>Comando commit</td>
-            <td>&nbsp;</td>
+            <td>Comando utilizado para adicionar a área commited e referenciar as alterações feitas atraves de uma breve descrição</td>
         </tr>
         <tr>
             <td>Comando chekout</td>
-            <td>&nbsp;</td>
+            <td></td>
         </tr>
         <tr>
             <td>&nbsp;</td>
